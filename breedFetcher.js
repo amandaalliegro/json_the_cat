c04ca6b8f50bb251d59f45f781d94316f36f8974
amandaalliegro <amanda.m.arnaut@gmail.com>
2020-09-24 20:13:45 +0000

    error if breed doesnt exist

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,12 +1,18 @@
 const request = require('request');
 const breed = process.argv[2];
 
+
+
 request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
   if (error) {
     throw error;
   }
+ 
   const data = JSON.parse(body);
-  console.log(data);
-  console.log(typeof data);
+  if (data[0]) {
+    console.log(data[0].description);
+  } else {
+    console.log(`Breed not found.`);
+  }
 }); 
 
