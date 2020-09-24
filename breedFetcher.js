091c4cb7985947276220d326d09c20674953b62d
amandaalliegro <amanda.m.arnaut@gmail.com>
2020-09-24 20:45:53 +0000

    eslinted

--- breedFetcher.js
+++ breedFetcher.js
@@ -6,20 +6,20 @@ if (breed === undefined) {
   process.exit();
 }
 const fetchBreedDescription = function(breedName, callback) {
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
   
-  if (error) {
-    console.log(error);
-    process.exit();
-  }
+    if (error) {
+      console.log(error);
+      process.exit();
+    }
  
-  const data = JSON.parse(body);
-  if (data[0]) {
-    console.log(data[0].description);
-  } else {
-    console.log(`Breed not found.`);
-  }
-});
-}
+    const data = JSON.parse(body);
+    if (data[0]) {
+      console.log(data[0].description);
+    } else {
+      console.log(`Breed not found.`);
+    }
+  });
+};
 
 module.exports = { fetchBreedDescription };
