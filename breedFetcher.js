d83e96a8dbffa738e7410d19bab60aef4f69d29b
amandaalliegro <amanda.m.arnaut@gmail.com>
2020-09-24 20:00:52 +0000

    implent the code so its possible to fetch breeds

---
+++ breedFetcher.js
@@ -0,0 +1,13 @@
+const request = require('request');
+const breed = process.argv[2];
+
+request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
+  if (error) {
+    throw error;
+  }
+  console.log(typeof body);
+}); 
+
+const data = JSON.parse(body);
+console.log(data);
+console.log(typeof data);
